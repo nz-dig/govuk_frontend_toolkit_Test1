7a342bca695dba7abe3070a4f78904cbbd38113d
nz-dig <naomi.zorn@digirati.co.uk>
2015-06-08 16:55:19 +0100

    first commit

---
+++ govuk_frontend_toolkit_Test1/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace govuk_frontend_toolkit_Test1
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
